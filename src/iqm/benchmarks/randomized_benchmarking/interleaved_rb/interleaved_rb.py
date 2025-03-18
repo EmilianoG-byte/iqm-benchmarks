40d63ed119fdfff7b0d185e8a3a41399586f2394
Pedro Figueroa-Romero <146174752+pedrofigro@users.noreply.github.com>
2025-03-18 11:10:45 +0100

    Add max circuits per batch parameter (#48)
    * benchmark.py: added max_circuits_per_batch configuration parameter
    
    * utils.py - added max_circuits_per_batch option parameter to submit_execute
    
    * utils.py - (fixed typo - a missing min) added max_circuits_per_batch option parameter to submit_execute
    
    * Update requirements
    
    * utils.py - fixed indentation issue in docstring
    
    * compressive_gst.py - added max_circuits_per_batch argument to submit_execute
    
    * ghz.py - added max_circuits_per_batch argument to submit_execute
    
    * qscore.py - added max_circuits_per_batch argument to submit_execute
    
    * clops.py - added max_circuits_per_batch argument to submit_execute
    
    * quantum_volume.py - added max_circuits_per_batch argument to submit_execute
    
    * mirror_rb.py - added max_circuits_per_batch argument to submit_execute
    
    * randomized_benchmarking_common.py - added max_circuits_per_batch argument to submit_execute
    
    * utils.py - updated submit_execute and black reformat
    
    * quantum_volume.py - changed simulator
    
    * Added unit test and removed return statement in submit_execute for no restrictions case
    
    * utils.py - fixed indent in docstring
    
    * updated readme and changelog
    
    * Update tests/unit/test_submit_execute.py
    
    Co-authored-by: Raphael <53472238+rabrie@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Pedro Figueroa <pedro.romero@meetiqm.com>
    Co-authored-by: Raphael Brieger <raphael.brieger@meetiqm.com>
    Co-authored-by: Raphael <53472238+rabrie@users.noreply.github.com>

--- src/iqm/benchmarks/randomized_benchmarking/interleaved_rb/interleaved_rb.py
+++ src/iqm/benchmarks/randomized_benchmarking/interleaved_rb/interleaved_rb.py
@@ -386,6 +386,7 @@ class InterleavedRandomizedBenchmarking(Benchmark):
                         self.shots,
                         self.calset_id,
                         self.max_gates_per_batch,
+                        self.configuration.max_circuits_per_batch,
                     )
                 )
                 all_rb_jobs["interleaved"].append(
@@ -397,6 +398,7 @@ class InterleavedRandomizedBenchmarking(Benchmark):
                         self.shots,
                         self.calset_id,
                         self.max_gates_per_batch,
+                        self.configuration.max_circuits_per_batch,
                     )
                 )
                 qcvv_logger.info(f"Both jobs for sequence length {seq_length} submitted successfully!")
@@ -492,6 +494,8 @@ class InterleavedRandomizedBenchmarking(Benchmark):
                         backend,
                         self.calset_id,
                         max_gates_per_batch=self.max_gates_per_batch,
+                        max_circuits_per_batch=self.configuration.max_circuits_per_batch,
+                        circuit_compilation_options=self.circuit_compilation_options,
                     )
                 )
                 all_rb_jobs["interleaved"].extend(
@@ -502,6 +506,8 @@ class InterleavedRandomizedBenchmarking(Benchmark):
                         backend,
                         self.calset_id,
                         max_gates_per_batch=self.max_gates_per_batch,
+                        max_circuits_per_batch=self.configuration.max_circuits_per_batch,
+                        circuit_compilation_options=self.circuit_compilation_options,
                     )
                 )
                 qcvv_logger.info(
