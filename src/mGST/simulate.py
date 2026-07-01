df6373e585053f552f8f239e86794283e822944e
EmilianoG-byte <emilianogodinez0518@gmail.com>
2026-07-01 18:45:52 +0200

    ran simulation for target 2Q reconstruction
    added some functions for the next test with perturbed operators

---
+++ src/mGST/simulate.py
@@ -0,0 +1,97 @@
+"""Functions used to simulate samples for testing purposes"""
+from mGST import additional_fns
+from mGST.low_level_jit import contract_mps_all_povm
+from mGST.utility_functions_comparisons import kraus_tensor_to_mgst
+from mGST.typing import Tensor, Matrix, Vector
+import jax.numpy as jnp
+
+def generate_sequence_indices(num_gates:int, num_circuits:int, seq_len_list:list[int], prune_negatives:bool=True) -> list[list[int]]:
+    """Generate a sequence of gate indices
+
+    This code is adpated from `compressive_gst.generate_meas_circuits`
+
+    Args:
+        num_gates: Number of gates in the circuit.
+        num_circuits: Number of circuits to generate.
+        seq_len_list: List of three integers representing the minimum, cut, and maximum sequence lengths.
+        prune_negatives: If True (default), we get rid of the -1 in the indices list.
+
+    Returns:
+        A list of lists, where each inner list contains the gate indices for a circuit.
+    """
+
+    # Calculate number of short and long circuits
+    N_short = int(jnp.ceil(num_circuits / 2))
+    N_long = int(jnp.floor(num_circuits / 2))
+    L_MIN, L_CUT, L_MAX = seq_len_list
+
+    gate_indices = additional_fns.random_seq_design(num_gates, L_MIN, L_CUT, L_MAX, N_short, N_long)
+    if prune_negatives:
+        gate_indices = [list(seq[seq >= 0]) for seq in gate_indices]
+    return gate_indices
+
+def compute_probability_matrices(gate_indices: list[list[int]], kraus_tensor:Tensor, povm_psd:Tensor, state_psd:Tensor, num_shots:int, seed:int=42) -> dict[str, Matrix]:
+    """Compute the exact and sampled probability matrices for a given set of gate indices and gate set (kraus, povm, and state).
+    
+    Args:
+        gate_indices: A list of lists, where each inner list contains the gate indices for a circuit.
+        kraus_tensor: Kraus tensor of shape (num_gates, kraus_rank, dim, dim).
+        povm_psd: POVM tensor of shape (num_povm_elements, povm_rank, dim).
+        state_psd: State tensor of shape (dim, state_rank).
+        num_shots: The number of shots to use for sampling.
+        seed: The random seed for sampling.
+
+    Returns:
+        A dictionary containing the exact and sampled probability matrices.
+    """
+
+    prob_matrix_exact = []
+    for indices in gate_indices:
+        prob_matrix_exact.append(jnp.real(contract_mps_all_povm(kraus_tensor, povm_psd, state_psd, indices)))
+        
+    prob_matrix_exact = jnp.array(prob_matrix_exact).T
+    prob_matrix_sampled = additional_fns.sampled_measurements(prob_matrix_exact, num_shots, seed=seed)
+    return {"exact": prob_matrix_exact, "sampled": prob_matrix_sampled}
+
+
+def get_perturbed_state(state_matrix: Matrix, rank:int, epsilon: float, seed: int=42) -> Vector:
+    """Get a perturbed state vector by applying a random Kraus operator to the original state vector.
+    
+    Args:
+        state_vect: The original state vector of shape (dim_in, dim_in*).
+        rank: The rank of the Kraus operator to be applied. This will be the new rank of the perturbed state as the original state is assumed to be rank 1.
+        epsilon: The perturbation strength.
+        seed: The random seed for generating the Kraus operator.
+        
+    Returns:
+        The perturbed state vector of shape (dim_in, dim_in*).
+    """
+    dim = state_matrix.shape[0]
+    kraus_perturbed = additional_fns.randKrausSet(1, dim, rank_kraus=rank, a=epsilon, seed=seed)
+    kraus_perturbed_superop = kraus_tensor_to_mgst(kraus_perturbed) # num_gates, dim_out^2, dim_in^2
+    # squeeze the num_gates out
+    kraus_perturbed_superop = kraus_perturbed_superop.squeeze(axis=0) # dim^2, dim^2
+    state_vect = state_matrix.flatten()
+    state_perturbed = jnp.dot(kraus_perturbed_superop, state_vect) # dim^2
+    return state_perturbed.reshape((dim, dim)) # dim_in, dim_in*
+
+def get_perturbed_povm(povm_tensor: Tensor, rank:int, epsilon: float, seed: int=42) -> Tensor:
+    """Get a perturbed POVM vector by applying a random Kraus operator to the original POVM vector.
+    
+    Args:
+        povm_tensor: The original POVM tensor of shape (num_povm, dim_out, dim_out*).
+        rank: The rank of the Kraus operator to be applied. This will be the new rank of the perturbed POVM as the original POVM is assumed to be rank 1.
+        epsilon: The perturbation strength.
+        seed: The random seed for generating the Kraus operator.
+        
+    Returns:
+        The perturbed POVM vector of shape (num_povm, dim_out, dim_out*).
+    """
+    num_povm, dim, dim = povm_tensor.shape
+    kraus_perturbed = additional_fns.randKrausSet(1, dim, rank_kraus=rank, a=epsilon, seed=seed)
+    kraus_perturbed_superop = kraus_tensor_to_mgst(kraus_perturbed) # num_gates, dim_out^2, dim_in^2*
+    # squeeze the num_gates out
+    kraus_perturbed_superop = kraus_perturbed_superop.squeeze(axis=0) # dim_out^2, dim_in^2*
+    povm_vect = povm_tensor.reshape((num_povm, dim*dim)) # num_povm, dim_out * dim_out*
+    povm_perturbed = jnp.einsum('ij, jk -> ik', povm_vect, kraus_perturbed_superop) # num_povm, dim_in * dim_in*
+    return povm_perturbed.reshape((num_povm, dim, dim)) # num_povm, dim_out, dim_out*
